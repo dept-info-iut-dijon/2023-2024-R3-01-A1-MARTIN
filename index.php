fe913127b84659bcb2b01a0a695e68101458e97d
Ruben martin <ruben.martin@iut-dijon.u-bourgogne.fr>
2023-10-23 17:18:54 +0200

    Commit du TP5

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+    require_once("controller/mainController.php");
+
+    $main_controller = new MainController();
+
+    $main_controller->Index();
+?>
