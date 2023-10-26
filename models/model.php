dabd52f32b503d8cf8f46dd1d223a1f404ffdc03
Ruben Martin <ruben.martin@iut-dijon.u-bourgogne.fr>
2023-10-26 11:42:10 +0200

    Ajout de model.php

---
+++ models/model.php
@@ -0,0 +1,21 @@
+<?php
+    abstract class Model{
+        private PDO $db;
+
+        protected function execRequest(string $sql, array $params = null) : PDOStatement|false {
+            $db = $this->getDB();
+            $statement = $db->prepare($sql);
+            return $statement->execute($params);
+        }
+
+        public function getDB() : PDO {
+            if($this->db == null){
+                $this->db = new PDO("mysql:host=localhost;dbname=grp-474_s3_progweb", "grp-474", "MyNNhkSH", array(
+                    PDO::ATTR_ERRMODE =>
+                    PDO::ERRMODE_EXCEPTION));
+                
+            }
+            return $this->db;
+        }
+    }
+?>
