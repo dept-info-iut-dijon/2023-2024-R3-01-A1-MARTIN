c5c695354e2a1e5e68587744675c057f840070ac
Ruben_Martin <ruben.martin@iut-dijon.u-bourgogne.fr>
2023-11-17 16:33:42 +0100

    Ajout de getAll et getById dans la classe pokemonManager.php

--- models/pokemonManager.php
+++ models/pokemonManager.php
@@ -1,7 +1,26 @@
 <?php
-    class PokemonManager {
+    class PokemonManager extends Model {
        public function getAll() : Array{
-            
+            $resultat = array();
+            $sql = "SELECT * FROM POKEMON";
+            $response = $this->execRequest($sql, []);
+
+            while($donnees = $response->fetch(PDO::FETCH_ASSOC)) {
+                $pokemon = new Pokemon();
+                $pokemon->hydrate($donnees);
+                $resultat[] = $pokemon;
+            }
+            return $resultat;
         }  
+
+        public function getId(int $id) : Pokemon {
+            $result = null;
+            $sql = "SELECT * FROM POKEMON WHERE idPokemon =? ";
+            $response = $this->execRequest($sql, array($id));
+            $pokemon = new Pokemon();
+            $pokemon->hydrate(array($response));
+            $result = $pokemon;
+            return $result;
+        }
     }
 ?>
